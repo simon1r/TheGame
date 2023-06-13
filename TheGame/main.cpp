498e0c27106d6361b6864e345774bd8154685991
simon1r <116090914+simon1r@users.noreply.github.com>
2023-06-13 13:25:32 +0200

    final
    victory screen, enemies

--- TheGame/main.cpp
+++ TheGame/main.cpp
@@ -16,9 +16,9 @@ using namespace std;
 
 sf::RenderWindow window(sf::VideoMode(1000, 1000), "Dungeon Scroller");
 sf::Vector2f cameraPos;
-bool dead;
+bool dead,win;
 
-class CustomRectangle : public sf::RectangleShape {//};, public sf::Sprite {
+class CustomRectangle : public sf::RectangleShape {
 public:
     float time =0;
     CustomRectangle(){}
@@ -75,12 +75,16 @@ public:
         else time += t;
 
         int d=4;
-        if(movement.x > 0) d = 2;
-        if(movement.x < 0) d = 1;
-        if(movement.y > 0) d =0 ;
-        if(movement.y < 0) d = 3;
 
-        setTexture(&tex[d][(int)(time * 10) % 6]      );
+
+        if(movement.x != 0 || movement.y!= 0)
+            if( abs(movement.x) > abs(movement.y))
+                if(movement.x > 0) d = 2;
+                else d = 1;
+            else
+                if(movement.y > 0) d = 0;
+                else d = 3;
+        setTexture(&tex[d][(int)(time * 10) % 6]);
 
 
         if (health <= 0) dead = true;
@@ -106,6 +110,7 @@ public:
     float dmg;
     sf::Vector2f velocity;
     int index;
+    bool players;
     Bullet(){};
     Bullet(float sx , float sy,    sf::Vector2f velocity,float dmg,sf::Texture &tex):CustomRectangle(sx,sy)
     {
@@ -121,14 +126,14 @@ public:
                 RemoveBullet(index);
                 return;
             }
-        if (collide(player)) {
+        if (!players && collide(player)) {
             player.health -= dmg;
             RemoveBullet(index);
             return;
         }
     }
 };
-const int maxBullets=100;
+const int maxBullets=200;
 Bullet bullets[maxBullets];
 
 void RemoveBullet(int n){
@@ -194,18 +199,79 @@ public:
     }
 };
 
-class Enemy : CustomRectangle{
+class Chest : public CustomRectangle{
 public:
-    sf::Vector2f a,b;
-    float speed;
-    float dmg;
-    Enemy(float sx , float sy, sf::Texture &tex):CustomRectangle(sx,sy)
+    Chest(float sx , float sy, sf::Texture &tex):CustomRectangle(sx,sy)
     {
-
         setTexture(&tex);
     }
-
+    void update(float t){
+        time += t;
+        if(collide(player)) win = true;
+    }
 };
+int enemyCount =0;
+sf::Texture enemyTex[4][8];
+class Enemy : public CustomRectangle{
+public:
+    float speed,targetDistance,dmg,health;
+
+    bool activ;
+    Enemy(){}
+    Enemy(float sx , float sy, float speed=0,float targetDistance =0,float dmg =0,float health=0):CustomRectangle(sx,sy)
+    {
+        this->speed=speed;
+        this->targetDistance=targetDistance;
+        this->dmg = dmg;
+        this->health = health;
+        // this->tex = tex;
+
+        setTexture(&enemyTex[0][0]);
+        enemyCount++;
+    }
+    void update(float t){
+        time += t;
+        sf::Vector2f dis =  player.getPosition() + player.getSize()/2.f - getPosition() - getSize()/2.f;
+        float distance = sqrt(dis.x*dis.x + dis.y * dis.y);
+        if(distance < targetDistance) activ = true;
+        if(activ){
+            tryMove((dis/distance*speed*t).x,0);
+            tryMove(0,(dis/distance*speed*t).y);
+            int d=4;
+            if(abs(dis.x) > abs(dis.y))
+                if(dis.x > 0) d = 3;
+                else d = 1;
+
+            else
+                if(dis.y > 0) d = 2;
+                else d = 0;
+
+
+            if(d!=4)setTexture(&enemyTex[d][(int)(time * 10) % 6]      );
+        }
+        if(collide(player)) player.health -= dmg *t;
+    }
+    void tryMove(float x,float y){
+        sf::Vector2f movement(x,y);
+        move(movement);
+        for (int i = 0; i < added; i++)
+            if (collide(m[i])) {
+                move(-movement);
+                break;
+            }
+    }
+};
+
+const int maxEnemies=200;
+Enemy enemies[maxEnemies];
+
+
+void RemoveEnemy(int n){
+    for (int i=n;i<enemyCount-1;i++)
+        enemies[i] = enemies[i + 1];
+
+    if (n<=enemyCount)enemyCount--;
+}
 
 class Saw : public CustomRectangle{
 public:
@@ -215,6 +281,7 @@ public:
     Saw(){}
     Saw(float sx , float sy, sf::Texture &tex,float dmg=0,float speed=0,float offset=0):CustomRectangle(sx,sy)
     {
+
         this->dmg = dmg;
         this->speed = speed;
         setTexture(&tex);
@@ -232,20 +299,38 @@ public:
 int main() {
 
 
-    sf::Texture brick,background,bulletTex,cannon,defeatTex;
+    sf::Texture brick,background,bulletTex,cannon,defeatTex,victoryTex, sawTex, chestTex,square;
+    chestTex.loadFromFile("chest.png");
+    sawTex.loadFromFile("saw.png");
     brick.loadFromFile("brick.jpg");
     bulletTex.loadFromFile("bullet.png");
     cannon.loadFromFile("cannon.png");
     background.loadFromFile("bg.jpg");
     defeatTex.loadFromFile("def.png");
+    victoryTex.loadFromFile("vic.png");
+    square.loadFromFile("square.png");
     background.setRepeated(true);
     CustomRectangle bg(3000,3000,background);
+
     CustomRectangle defeat(1000,500,defeatTex);
-    defeat.setFillColor(sf::Color(200,20,50));
     defeat.setPosition(0,160);
-    //bg.setScale(0.1,0.1);
+
+    CustomRectangle victory(1000,500,victoryTex);
+    victory.setPosition(0,160);
     bg.setTextureRect({ 0, 0, 7000, 7000 });
 
+
+    CustomRectangle health(1000,50,square);
+    health.setFillColor(sf::Color(255,0,0));
+
+    sf::Text timer;
+    sf::Font font;
+    font.loadFromFile("font.otf");
+    timer.setFont(font);
+    timer.setPosition(200,530);
+    timer.setScale(3,3);
+
+    //Map (walls) generation
     for (int x = 0; x < sizeX; x++) {
         for (int y = 0; y < sizeX; y++) {
             if (x == 0 || y == 0 || y == sizeX - 1 || x == sizeX - 1 || (x == 15 && y < 10)
@@ -261,25 +346,35 @@ int main() {
         }
     }
 
+    //Victory point
+    Chest chest(200,200,chestTex);
+    chest.setPosition(400,2300);
 
+
+
+    //Cannons
     int cannonCount = 8;
     Cannon cannons[cannonCount];
     Bullet cannonBullet(50,50,sf::Vector2f(0,-100),0.2,bulletTex);
-    for(int i=0;i<4;i++) {
-        Cannon c(210/3.0, 260/3.0, cannon, cannonBullet ,1,i);
+    for(int i=0;i<cannonCount;i++) {
+        Cannon c(210/3.0, 260/3.0, cannon, cannonBullet ,4,i);
         c.setPosition(400 * i + 400, 300);
+        //from 0 to 3 rotates to right 90deg
+        c.rot(i%3);
         cannons[i] = c;
     }
 
-    for(int i=0;i<4;i++) {
-        Cannon c(210/3.0, 260/3.0, cannon, cannonBullet ,1,i);
-        c.setPosition(400 * i + 400, 300);
-        c.rot(i);
-        cannons[i+4] = c;
-    }
+    //Enemies spawn
+    for (int y = 0; y < 4; y++)
+        for (int x = 0; x < 6; x++)
+            enemyTex[y][x].loadFromFile("enemy.png", sf::IntRect( 512/8 * x, 256/4 * y, 512/8, 256/4));
 
-    sf::Texture sawTex;
-    sawTex.loadFromFile("saw.png");
+    for(int i =0;i<10;i++){
+        Enemy en(100,100,200,200,0.1,1);
+        en.setPosition(400 + i* 300,400);
+        enemies[i] = en;
+    }
+    //Saws
     int sawCount = 8;
     Saw saws[sawCount];
     for(int i=0;i<sawCount;i++) {
@@ -291,6 +386,7 @@ int main() {
 
     sf::Clock clock;
     bool shoot;
+    //main loop
     while (window.isOpen()) {
         float t = clock.restart().asSeconds();
 
@@ -303,19 +399,29 @@ int main() {
                 return 0;
             }
         }
-        if(!dead){
+        if(!dead && !win){
             if( sf::Keyboard::isKeyPressed(sf::Keyboard::E)){
                 if(!shoot &&bulletCount < maxBullets) {
                     shoot = true;
                     sf::Vector2f dir = sf::Vector2f(sf::Mouse::getPosition(window).x , sf::Mouse::getPosition(window).y )+cameraPos - player.getPosition() ;
                     dir = dir/sqrt(dir.x * dir.x + dir.y * dir.y);
-                    bullets[bulletCount] =  Bullet(40,40,dir * 500.f,10,bulletTex);
+                    bullets[bulletCount] =  Bullet(40,40,dir * 500.f,0.2,bulletTex);
                     bullets[bulletCount].index = bulletCount;
+                    bullets[bulletCount].players = true;
                     bullets[bulletCount].setPosition(player.getPosition());
                     bulletCount++;
-                }}
+                }
+            }
             else shoot = false;
 
+            for(int i=0;i<enemyCount;i++){
+                for(int j=0;j<bulletCount;j++)
+                    if(bullets[j].players && enemies[i].collide(bullets[j])){
+                        enemies[i].health -= bullets[j].dmg;
+                        RemoveBullet(bullets[j].index);
+                        if(enemies[i].health <= 0) RemoveEnemy(i);
+                    }
+            }
 
             for (int i = 0; i < bulletCount; i++)bullets[i].update(t);
 
@@ -324,10 +430,11 @@ int main() {
             sf::Vector2f dis =  player.getPosition() - cameraPos - sf::Vector2f (500,500);
             cameraPos += dis * (5* t) ;
 
-
+            chest.update(t);
+            for (int i = 0; i < enemyCount; i++) enemies[i].update(t);
             for (int i = 0; i < cannonCount; i++)  cannons[i].update(t);
             for (int i = 0; i < sawCount; i++)  saws[i].update(t);
-            window.clear();
+            window.clear(sf::Color(10,10,30));
 
         }
         sf::View view(sf::FloatRect(cameraPos.x, cameraPos.y, 1000.f, 1000.0));
@@ -339,25 +446,36 @@ int main() {
             window.draw(m[i]);
 
 
+
         for (int i = 0; i < cannonCount; i++) window.draw(cannons[i]);
         for (int i = 0; i < sawCount; i++) window.draw(saws[i]);
+        window.draw(chest);
 
+        for (int i = 0; i < bulletCount; i++)  window.draw(bullets[i]);
 
-        for (int i = 0; i < bulletCount; i++) {
-            window.draw(bullets[i]);
-        }
-
-
-
-
+        for (int i = 0; i < enemyCount; i++)  window.draw(enemies[i]);
         window.draw(player);
-        if(dead){
-            sf::View view(sf::FloatRect(0,0, 1000.f, 1000.0));
-            window.setView(view);
-            window.draw(defeat);
+
+
+        view = sf::View( sf::FloatRect(0,0, 1000.f, 1000.0));
+        window.setView(view);
+
+        if(player.health > 0)
+            health.setScale(player.health,1);
+        else health.setScale(0,1);
+        window.draw(health);
+
+        if(dead||win){
+            timer.setString("Time: " + to_string((int)(chest.time)) + "."  + to_string((int)((chest.time - (int)chest.time) * 100.0f)) + " seconds");
+
+            if(win)window.draw(victory);
+            else window.draw(defeat);
+            window.draw(timer);
+            window.display();
         }
-        window.display();
-        // std::this_thread::sleep_for(std::chrono::milliseconds(1));
+
+        if(!dead && !win)
+            window.display();
     }
     return 0;
 }
